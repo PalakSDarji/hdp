156a6170a5234b5bfdcc4245469cee3838241455
palak <palak.developer.amax@gmail.com>
2016-08-17 09:26:45 +0530

    c3

--- app/src/main/java/com/hadippa/activities/ProfileActivity.java
+++ app/src/main/java/com/hadippa/activities/ProfileActivity.java
@@ -1,7 +1,9 @@
 package com.hadippa.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.hadippa.R;
 
@@ -12,6 +14,13 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        findViewById(R.id.ivEdit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this,EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
